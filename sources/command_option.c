c58c798b9e6c5cd64187cc172c333e17e90a178e
Matis <matis.mikael@gmail.com>
2025-11-18 11:51:20 +0100

    fix help and don't forget to fix project name in makefile

--- sources/command_option.c
+++ sources/command_option.c
@@ -5,20 +5,22 @@
 #include <getopt.h>
 
 static void print_help(){
-	printf(
-    "-h ou --help : affiche l'aide du programme ainsi que la syntaxe d'exécution du programme\n"
-    "--dry-run : test l'accès à la liste des processus sur la machine locale et/ou distante sans les afficher\n"
-    "-c ou --remote-config : spécifie le chemin vers le fichier de configuration contenant les informations de connexion sur les machines distantes\n"
-    "-t ou --connexion-type : spécifie le type de connexion à utiliser pour la connexion sur les machines distantes (ssh, telnet)\n"
-    "-P ou --port : spécifie le port à utiliser pour le type de connexion choisi.
-    Si cette option n'est pas spécifié, alor le port par défaut du type de connexion est choisi\n"
-    "-l ou --login : spécifie l'identifiant de connexion et la machine distante.
-    Ex : --login user@remote_server. remote_server est soit l'IP ou le nom DNS de la machine distante\n"
-    "-s ou --remote-server : spécifie le nom DNS ou l'IP de la machine distante\n"
-    "-u ou --username : spécifie le nom de l'utilisateur à utiliser pour la connexion\n"
-    "-p ou --password : spécifie le mot de passe à utiliser pour la connexion\n"
-    "-a ou --all : spécifie au programme de collecter à la fois la liste des processus sur la machine local et les machines distantes.
-    S'utilise uniquement si l'option -c ou -s est utilisé.\n");
+
+	const char* help_message = "-h ou --help : affiche l'aide du programme ainsi que la syntaxe d'exécution du programme\n"
+   	 "--dry-run : test l'accès à la liste des processus sur la machine locale et/ou distante sans les afficher\n"
+   	 "-c ou --remote-config : spécifie le chemin vers le fichier de configuration contenant les informations de connexion sur les machines distantes\n"
+   	 "-t ou --connexion-type : spécifie le type de connexion à utiliser pour la connexion sur les machines distantes (ssh, telnet)\n"
+   	 "-P ou --port : spécifie le port à utiliser pour le type de connexion choisi. \
+   	 Si cette option n'est pas spécifié, alor le port par défaut du type de connexion est choisi\n"
+   	 "-l ou --login : spécifie l'identifiant de connexion et la machine distante. \
+   	 Ex : --login user@remote_server. remote_server est soit l'IP ou le nom DNS de la machine distante\n"
+   	 "-s ou --remote-server : spécifie le nom DNS ou l'IP de la machine distante\n"
+   	 "-u ou --username : spécifie le nom de l'utilisateur à utiliser pour la connexion\n"
+   	 "-p ou --password : spécifie le mot de passe à utiliser pour la connexion\n"
+   	 "-a ou --all : spécifie au programme de collecter à la fois la liste des processus sur la machine local et les machines distantes. \
+   	 S'utilise uniquement si l'option -c ou -s est utilisé.\n";
+
+	printf("%s", help_message);
 }
 
 int command_run(int argc, char *argv[]){
